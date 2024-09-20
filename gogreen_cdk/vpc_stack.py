88f77741d1b8fcfc27dbecf8a88727131032313b
Lock Yew <lockyew@Locks-MacBook-Air.local>
2024-09-20 21:27:12 +0800

    update VPC

--- gogreen_cdk/vpc_stack.py
+++ gogreen_cdk/vpc_stack.py
@@ -10,10 +10,13 @@ class VpcStack(cdk.Stack):
 
         # Create a VPC with 2 public and 6 private subnets
         self.vpc = ec2.Vpc(self, "GoGreenVpc",
-            max_azs=2,
+            max_azs=2,  # Use 2 Availability Zones
             subnet_configuration=[
                 ec2.SubnetConfiguration(name="PublicSubnet", subnet_type=ec2.SubnetType.PUBLIC, cidr_mask=24),
-                ec2.SubnetConfiguration(name="PrivateSubnet", subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT, cidr_mask=24),
+                ec2.SubnetConfiguration(name="WebTier", subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT, cidr_mask=24),  # Private subnet for web tier
+                ec2.SubnetConfiguration(name="AppTier", subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT, cidr_mask=24),  # Private subnet for app tier
+                ec2.SubnetConfiguration(name="DatabaseTier", subnet_type=ec2.SubnetType.PRIVATE_ISOLATED, cidr_mask=24),  # Private isolated subnet for database
             ],
-            nat_gateways=1
+            nat_gateways=2  # 1 NAT gateway per AZ
         )
+
